518fd762886ca84a9511a6cf1cea5fe8253b07c0
ylw2011 <57453886+ylw2011@users.noreply.github.com>
2025-06-06 14:42:34 +0800

    Add files via upload

--- chatgpt.py
+++ chatgpt.py
@@ -10,21 +10,26 @@ class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
         self.model = os.getenv("OPENAI_MODEL", default = "gpt-4o-mini")
-        #self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
-        #self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
-        #self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
-        #self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
+        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0.5))
+        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+        system_prompt = "參考下列內容，回答問題："
+        with open("kw.txt", "r", encoding="utf-8") as f:
+            for line in f:
+                system_prompt += line.strip()  # 去掉每行的換行
 
     def get_response(self):
         response = openai.chat.completions.create(
         model=self.model,
         messages=[
+            {"role": "system", "content": system_prompt},
             {"role": "user", "content": self.prompt.generate_prompt()}
         ],
-        #temperature=self.temperature,
-        #frequency_penalty=self.frequency_penalty,
-        #presence_penalty=self.presence_penalty,
-        #max_tokens=self.max_tokens
+        temperature=self.temperature,
+        frequency_penalty=self.frequency_penalty,
+        presence_penalty=self.presence_penalty,
+        max_tokens=self.max_tokens
         )
         return response.choices[0].message.content.strip()
 
