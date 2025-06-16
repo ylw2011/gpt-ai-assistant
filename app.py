5b44937f388ec39e942d579638006eee3284be9f
ylw2011 <57453886+ylw2011@users.noreply.github.com>
2025-06-16 13:59:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -58,13 +58,12 @@ def handle_message(event):
         return
 
     working_status = True
-    app.logger.info("Process "+event.message.text)
-    line_bot_api.reply_message(ReplyMessageRequest(reply_token=event.reply_token,messages=[TextMessage(text=\"{\"type\": \"flex\",\"altText\": \"測試\",\"contents\": {\"type\": \"bubble\",\"body\": {\"type\": \"box\",\"layout\": \"vertical\",\"contents\": [{\"type\": \"text\",\"text\": \"測試\",\"weight\": \"bold\",\"size\": \"xl\"}]}}}
-")]))
+    app.logger.info("Process "+event.message.text)    
     if working_status:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
+        reply_msg="{\"type\": \"flex\",\"altText\": \"測試\",\"contents\": {\"type\": \"bubble\",\"body\": {\"type\": \"box\",\"layout\": \"vertical\",\"contents\": [{\"type\": \"text\",\"text\": \"測試\",\"weight\": \"bold\",\"size\": \"xl\"}}}}"       
         line_bot_api.reply_message(ReplyMessageRequest(reply_token=event.reply_token,messages=[TextMessage(text=reply_msg)]))
 
 
