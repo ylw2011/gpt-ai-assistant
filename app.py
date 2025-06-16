afef316440c5d5abcadf422ee087cce8b805aa05
ylw2011 <57453886+ylw2011@users.noreply.github.com>
2025-06-16 14:20:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -64,7 +64,10 @@ def handle_message(event):
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
         reply_msg="{\"type\": \"bubble\",\"body\": {\"type\": \"box\",\"layout\": \"vertical\",\"contents\": [{\"type\": \"text\",\"text\": \"hello, world\"}])}"       
-        line_bot_api.reply_message(ReplyMessageRequest(reply_token=event.reply_token,messages=[TextMessage(text=reply_msg)]))
+        bubble_container = BubbleContainer(**reply_msg)
+        flex_message = FlexSendMessage(alt_text="Hello Ntcu", contents=bubble_container)
+        line_bot_api.reply_message(event.reply_token,flex_message)
+        #line_bot_api.reply_message(ReplyMessageRequest(reply_token=event.reply_token,messages=[TextMessage(text=reply_msg)]))
 
 
 if __name__ == "__main__":
